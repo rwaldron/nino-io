d18d20f9bee5970d04a4e4ba45081dbbee4caf82
Rick Waldron <waldron.rick@gmail.com>
2014-11-11 12:12:24 -0500

    Adds error message for invalid PWM access/use. Adds extensive tests for analogWrite & servoWrite

--- test/nino-io.js
+++ test/nino-io.js
@@ -7,12 +7,12 @@ var sinon = require("sinon");
 var fsStub = {
   chmod: function(path, flag, cb) {
     if (cb) {
-      cb(null, "[[descriptor]]");
+      cb(null, "[[mock descriptor]]");
     }
   },
   open: function(path, flag, cb) {
     if (cb) {
-      cb(null, "[[descriptor]]");
+      cb(null, "[[mock descriptor]]");
     }
   },
   read: function(fd, buffer, start, offset, position, cb) {
@@ -23,12 +23,16 @@ var fsStub = {
   readFile: function(path, encoding, cb) {
     cb(null, path, encoding);
   },
+  write: function(fd, buffer, start, offset, position, cb) {
+    if (cb) {
+      cb(null, "Success!");
+    }
+  },
   writeFile: function(path, encoding, cb) {
     if (cb) {
       cb(null, "Success!");
     }
   },
-
   writeFileSync: function(path, encoding, cb) {
     if (cb) {
       cb(null, "Success!");
@@ -338,6 +342,211 @@ exports["Nino.prototype.digitalRead"] = {
 };
 
 
+exports["Nino.prototype.analogWrite"] = {
+  setUp: function(done) {
+    this.clock = sinon.useFakeTimers();
+
+    this.fsWrite = sinon.spy(fsStub, "write");
+    this.write = sinon.spy(Pin.prototype, "write");
+    this.nino = new Nino();
+
+    done();
+  },
+  tearDown: function(done) {
+    restore(this);
+    Nino.reset();
+    done();
+  },
+
+  mode: function(test) {
+    test.expect(2);
+
+    var value = 255;
+    var pin = 11;
+
+    this.nino.pinMode(11, this.nino.MODES.PWM);
+    test.equal(this.nino.pins[11].mode, 3);
+    test.equal(this.nino.pins[11].isPwm, true);
+
+    test.done();
+  },
+
+  write: function(test) {
+    test.expect(18);
+
+    var value = 255;
+    var pin = 11;
+
+    this.nino.analogWrite(11, value);
+
+    // Internal write is called
+    test.equal(this.write.callCount, 1);
+    test.equal(this.write.args[0][0], value);
+
+    // Value is stored in pin state
+    test.equal(this.nino.pins[11].value, value);
+
+    // fs.write call 1 is called with expected arguments
+    test.equal(this.fsWrite.callCount, 1);
+    /*
+      [0] descriptor
+      [1] buffer value
+      [2] offset of buffer
+      [3] length of buffer
+      [4] start position in file
+      [5] callback (untested)
+     */
+    test.equal(this.fsWrite.args[0][0], "[[mock descriptor]]");
+    test.equal(this.fsWrite.args[0][1], new Buffer("5000000").toString());
+    test.equal(this.fsWrite.args[0][2], 0);
+    test.equal(this.fsWrite.args[0][3], "5000000".length);
+    test.equal(this.fsWrite.args[0][4], 0);
+
+
+    value = 0;
+
+    this.nino.analogWrite(11, value);
+
+    // Internal write is called
+    test.equal(this.write.callCount, 2);
+    test.equal(this.write.args[1][0], value);
+
+    // Value is stored in pin state
+    test.equal(this.nino.pins[11].value, value);
+
+    // fs.write call 2 is called with expected arguments
+    test.equal(this.fsWrite.callCount, 2);
+    /*
+      [0] descriptor
+      [1] buffer value
+      [2] offset of buffer
+      [3] length of buffer
+      [4] start position in file
+      [5] callback (untested)
+     */
+    test.equal(this.fsWrite.args[1][0], "[[mock descriptor]]");
+    test.equal(this.fsWrite.args[1][1], new Buffer("0").toString());
+    test.equal(this.fsWrite.args[1][2], 0);
+    test.equal(this.fsWrite.args[1][3], "0".length);
+    test.equal(this.fsWrite.args[1][4], 0);
+
+    test.done();
+  },
+
+  stored: function(test) {
+    test.expect(1);
+
+    var value = 255;
+    this.nino.analogWrite(11, value);
+
+    test.equal(this.nino.pins[11].value, value);
+
+    test.done();
+  }
+};
+
+exports["Nino.prototype.servoWrite"] = {
+  setUp: function(done) {
+    this.clock = sinon.useFakeTimers();
+
+    this.fsWrite = sinon.spy(fsStub, "write");
+    this.write = sinon.spy(Pin.prototype, "write");
+    this.nino = new Nino();
+
+    done();
+  },
+  tearDown: function(done) {
+    restore(this);
+    Nino.reset();
+    done();
+  },
+
+  mode: function(test) {
+    test.expect(2);
+
+    var value = 255;
+    var pin = 11;
+
+    this.nino.pinMode(11, this.nino.MODES.SERVO);
+    test.equal(this.nino.pins[11].mode, 4);
+    test.equal(this.nino.pins[11].isPwm, true);
+
+    test.done();
+  },
+
+  write: function(test) {
+    test.expect(18);
+
+    var value = 255;
+    var pin = 11;
+
+    this.nino.servoWrite(11, value);
+
+    // Internal write is called
+    test.equal(this.write.callCount, 1);
+    test.equal(this.write.args[0][0], value);
+
+    // Value is stored in pin state
+    test.equal(this.nino.pins[11].value, value);
+
+    // fs.write call 1 is called with expected arguments
+    test.equal(this.fsWrite.callCount, 1);
+    /*
+      [0] descriptor
+      [1] buffer value
+      [2] offset of buffer
+      [3] length of buffer
+      [4] start position in file
+      [5] callback (untested)
+     */
+    test.equal(this.fsWrite.args[0][0], "[[mock descriptor]]");
+    test.equal(this.fsWrite.args[0][1], new Buffer("2400000").toString());
+    test.equal(this.fsWrite.args[0][2], 0);
+    test.equal(this.fsWrite.args[0][3], "2400000".length);
+    test.equal(this.fsWrite.args[0][4], 0);
+
+
+    value = 0;
+
+    this.nino.analogWrite(11, value);
+
+    // Internal write is called
+    test.equal(this.write.callCount, 2);
+    test.equal(this.write.args[1][0], value);
+
+    // Value is stored in pin state
+    test.equal(this.nino.pins[11].value, value);
+
+    // fs.write call 2 is called with expected arguments
+    test.equal(this.fsWrite.callCount, 2);
+    /*
+      [0] descriptor
+      [1] buffer value
+      [2] offset of buffer
+      [3] length of buffer
+      [4] start position in file
+      [5] callback (untested)
+     */
+    test.equal(this.fsWrite.args[1][0], "[[mock descriptor]]");
+    test.equal(this.fsWrite.args[1][1], new Buffer("560000").toString());
+    test.equal(this.fsWrite.args[1][2], 0);
+    test.equal(this.fsWrite.args[1][3], "560000".length);
+    test.equal(this.fsWrite.args[1][4], 0);
+
+    test.done();
+  },
+
+  stored: function(test) {
+    test.expect(1);
+
+    var value = 255;
+    this.nino.analogWrite(11, value);
+
+    test.equal(this.nino.pins[11].value, value);
+
+    test.done();
+  }
+};
 
 exports["Nino.prototype.pinMode (analog)"] = {
   setUp: function(done) {
@@ -432,6 +641,27 @@ exports["Nino.prototype.pinMode (pwm/servo)"] = {
     this.nino.pinMode(3, 4);
     test.equal(this.nino.pins[3].mode, 4);
     test.equal(this.nino.pins[3].isPwm, true);
+
+    test.done();
+  },
+  modeInvalid: function(test) {
+    test.expect(4);
+
+    test.throws(function() {
+      this.nino.pinMode(12, this.nino.MODES.PWM);
+    }.bind(this));
+
+    test.throws(function() {
+      this.nino.analogWrite(12, 255);
+    }.bind(this));
+
+    test.throws(function() {
+      this.nino.pinMode(12, this.nino.MODES.SERVO);
+    }.bind(this));
+
+    test.throws(function() {
+      this.nino.servoWrite(12, 255);
+    }.bind(this));
 
     test.done();
   },
